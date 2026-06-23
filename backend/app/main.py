a35ce50d57d1290102fa6aec03d764543520c947
rowanbreakey <rowan.breakey@gmail.com>
2026-06-22 21:53:17 -0400

    additional processing for video
    calculates velocities of wrists and left eye so that down the line the ai will be able to tell if hands and gaze are moving. still needs some refinement.

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,6 +8,7 @@ from mediapipe.tasks.python import vision
 from mediapipe.tasks.python import audio
 import cv2 
 import numpy as np
+import math
 
 app = FastAPI()
 
@@ -29,12 +30,16 @@ class dataBatch(BaseModel):
     audio: str
 
 def do_video_analysis(frame_batch):
+    # good start just need to clean up my math portion a little so that it also checks to make sure everything exists and isnt none.
+    # use some if statements and set to 0.0 otherwise to appear as no movement.
+    # could also try doing something where if the landmarks arent detected it prompts the user to go back onscreen?
     data_by_frame = []
 
     base_options = python.BaseOptions(model_asset_path='holistic_landmarker.task')
 
     options = vision.HolisticLandmarkerOptions(
         base_options = base_options,
+        refine_landmarks = True,
         output_face_blendshapes = True,
         output_hand_landmarks = True
     )
@@ -52,28 +57,50 @@ def do_video_analysis(frame_batch):
                 "right_eye" : None
             }
 
-            if result.right_hand_landmarks:
+            if result.right_hand_landmarks and len(result.right_hand_landmarks) > 0:
                 wrist = result.right_hand_landmarks[0][0]
-                frame_data["right_wrist"] = (wrist.x, wrist.y, wrist.z)
+                frame_data["right_wrist"] = (wrist.x, wrist.y)
 
-            if result.left_hand_landmarks:
+            if result.left_hand_landmarks and len(result.left_hand_landmarks) > 0:
                 wrist = result.left_hand_landmarks[0][0]
-                frame_data["left_wrist"] = (wrist.x, wrist.y, wrist.z)
+                frame_data["left_wrist"] = (wrist.x, wrist.y)
 
-            if result.face_landmarks:
+            if result.face_landmarks and len(result.face_landmarks) > 0:
+                nose = result.face_landmarks[0][1]
                 eye_center = result.face_landmarks[0][468]
-                frame_data["right_eye"] = (eye_center.x, eye_center.y, eye_center.z)
+
+                if nose and eye_center:
+                    frame_data["left_eye"] = [eye_center.x-nose.x, eye_center.y-nose.y]
 
             data_by_frame.append(frame_data)
 
-    # now all the data is in the data_by_frame list so I just need to do some math and stuff to tell when the wrists are moving and where the eyes are looking at
+    velos_by_frame = []
 
+    for i in range(1, len(data_by_frame)):
+        dx_right_wrist = data_by_frame[i]["right_wrist"][0] - data_by_frame[i-1]["right_wrist"][0]
+        dy_right_wrist = data_by_frame[i]["right_wrist"][1] - data_by_frame[i-1]["right_wrist"][1]
+        right_wrist_step = round(math.sqrt(dx_right_wrist**2 + dy_right_wrist**2), 4)
 
+        dx_left_wrist = data_by_frame[i]["left_wrist"][0] - data_by_frame[i-1]["left_wrist"][0]
+        dy_left_wrist = data_by_frame[i]["left_wrist"][1] - data_by_frame[i-1]["left_wrist"][1]
+        left_wrist_step = round(math.sqrt(dx_left_wrist**2 + dy_left_wrist**2), 4)
 
+        dx_left_eye = data_by_frame[i]["left_eye"][0] - data_by_frame[i-1]["left_eye"][0]
+        dy_left_eye = data_by_frame[i]["left_eye"][1] - data_by_frame[i-1]["left_eye"][1]
+        left_eye_step = round(math.sqrt(dx_left_eye**2 + dy_left_eye**2), 4)
+
+        velos_by_frame.append((right_wrist_step, left_wrist_step, left_eye_step))
+    
+    return velos_by_frame
+
+    
+def do_audio_analysis(audio_batch):
+    return audio_batch
 
 @app.post("/api/process-batch")
 async def process_batch(batch: dataBatch):
-    do_video_analysis(batch.frames)
+    video_data = do_video_analysis(batch.frames)
+    audio_data = do_audio_analysis(batch.audio)
 
     eye_contact_score = "good"
     gesture_score = "excellent"
