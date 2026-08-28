54eefe0e61088f48b131d5c45aabf1e94be44a75
rowanbreakey <rowan.breakey@gmail.com>
2026-08-28 11:01:44 -0400

    feat: add slide control buttons

--- frontend/src/components/PresentationArea.jsx
+++ frontend/src/components/PresentationArea.jsx
@@ -37,14 +37,14 @@ function PresentationArea({children}) {
 
     const incrementPage = () => {
         if (page < maxPages) {
-            setPage((prev) => prev++);
+            setPage((prev) => prev + 1);
         }
     }
 
     const decrementPage = () => {
         if (page > 1) {
-            setPage((prev) => prev--);
-        };
+            setPage((prev) => prev - 1);
+        }
     }
 
     function selectButtonClicked() {
@@ -63,22 +63,28 @@ function PresentationArea({children}) {
         setPresentation(file);
     }
 
-    function onLoad({maxPages}) {
-        setMaxPages(maxPages);
+    function onLoad({numPages}) {
+        setMaxPages(numPages);
         setPage(1);
     }
 
     return(
         <>
-            <input type="file" accept="application/pdf" ref={fileInputRef} onChange={presentationUploaded} style={{display:"none"}}/>
+            <input type="file" accept="application/pdf" ref={fileInputRef} onClick={selectButtonClicked} onChange={presentationUploaded} style={{display:"none"}}/>
             {presentation ? (
-                <div className="ratio ratio-16x9 bg-dark rounded-4 shadow overflow-hidden" ref={containerRef}>
-                    <Document file={presentation} onLoadSuccess={onLoad}>
-                        <Page pageNumber={page} width={containerWidth}>
-                            {children}
-                        </Page>
-                    </Document>
-                </div>
+                <>
+                    <div className="ratio ratio-16x9 bg-dark rounded-4 shadow overflow-hidden" ref={containerRef}>
+                        <Document file={presentation} onLoadSuccess={onLoad}>
+                            <Page pageNumber={page} width={containerWidth}>
+                                {children}
+                                <div className='position-absolute bottom-0 end-0 p-2'>
+                                    <button onClick={decrementPage} className='btn btn-primary btn-sm rounded-circle' style={{ position: 'relative', zIndex: 10, pointerEvents: 'auto' }}>&lt;</button>
+                                    <button onClick={incrementPage} className='btn btn-primary btn-sm rounded-circle' style={{ position: 'relative', zIndex: 10, pointerEvents: 'auto' }}>&gt;</button>
+                                </div>
+                            </Page>
+                        </Document>
+                    </div>
+                </>
             ) : (
                 <button onClick={selectButtonClicked} className="ratio ratio-16x9 bg-dark rounded-4 shadow">
                     {children}
